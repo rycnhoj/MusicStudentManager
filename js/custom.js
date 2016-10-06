e043b75f2ec5e1fa3c258fd1d7d3c25c976a3b81
rycnhoj <rycnhoj@gmail.com>
2016-10-06 15:01:15 -0400

    index.html - Updates to formating of code - Moved active checkbox inline with instrument radio buttons
    js/custom.js
    - added comments
    - added DOMContentLoaded eventlistener
    - added boolean instrumentRadioCheck function for check if any of the radio buttons have been selected
    - added return true statement to dataCheck function when all data properly entered

--- js/custom.js
+++ js/custom.js
@@ -7,42 +7,66 @@
 (function() {
 "use strict";
 
-var firstname = document.getElementById('first-name');
-var lastname = document.getElementById('last-name');
-var age = document.getElementById('age');
-var type = document.getElementById('select-person-type');
-var instrument = document.querySelector('input[name=inst-radio]:checked');
-var active = document.getElementById('is-active-checkbox');
+  // Input fields
+  var firstname = document.getElementById('first-name');
+  var lastname = document.getElementById('last-name');
+  var age = document.getElementById('age');
+  var type = document.getElementById('select-person-type');
+  var instruments = document.querySelectorAll('input[name=inst-radio]');
+  var active = document.getElementById('is-active-checkbox');
+
+  // On document load
+  document.addEventListener('DOMContentLoaded', function() {
+      document.getElementById('add-person-form').addEventListener('submit', dataCheck);
 
 
-document.getElementById('add-person-form').addEventListener('submit', dataCheck);
+  });
 
-function dataCheck(event) {
-    event.preventDefault();
+  // Determines if any of the instrumetn radio buttons have been selected
+  // returns true if one has been selected
+  function instrumentRadioCheck() {
+    var radioCount = instruments.length;
 
-    if (firstname.value === '') {
-      alert('Please provide a first name.')
-      firstname.focus();
-    }
-    else if (lastname.value === '') {
-      alert('Please provide a last name.')
-      lastname.focus();
-    }
-    else if (age.value === '') {
-       alert('Please provide an age.')
-       age.focus();
-   }
-    else if (type.selectedIndex == 0) {
-      alert('Please select a person type.')
-      type.focus();
-    }
-    else if (instrument == null) {
-      alert('Please select an instrument.')
-    } else {
-      console.log('Form Submitted!');
+    for(var rbtn = 0; rbtn < radioCount; rbtn++)
+    {
+      if (instruments[rbtn].checked) {
+        return true;
+      }
     }
 
+    return false;
+  };
 
-}
+  function dataCheck(event) {
+      event.preventDefault();
+
+      if (firstname.value === '') {
+        alert('Please provide a first name.');
+        firstname.focus();
+        return false;
+      }
+      else if (lastname.value === '') {
+        alert('Please provide a last name.');
+        lastname.focus();
+        return false;
+      }
+      else if (age.value === '') {
+         alert('Please provide an age.');
+         age.focus();
+         return false;
+     }
+      else if (type.selectedIndex == 0) {
+        alert('Please select a person type.');
+        type.focus();
+        return false;
+      }
+      else if (!instrumentRadioCheck()) {
+        alert('Please select an instrument.');
+        return false;
+      } else {
+        console.log('Form Ready to Submit!');
+        return true;
+      }
+  };
 
 })();
